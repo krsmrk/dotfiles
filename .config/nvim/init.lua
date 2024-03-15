fb46d5744ea13620d9bb248ab15dcbcab3839d3b
Stefan <stefan.krusemark@codecentric.de>
2024-03-15 10:03:11 +0100

    neovim: Unify plugin import

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,54 +84,48 @@ mason_lspconfig.setup_handlers {
 	end
 }
 
-require("lazy").setup({
-  "neovim/nvim-lspconfig",
-  "elentok/format-on-save.nvim",
-})
-
 local lspconfig = require('lspconfig')
 
-lspconfig.terraformls.setup{}
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  pattern = {"*.tf", "*.tfvars"},
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+lspconfig.terraformls.setup {}
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = { "*.tf", "*.tfvars", "*.hcl" },
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
 
 local format_on_save = require("format-on-save")
 local formatters = require("format-on-save.formatters")
 
 format_on_save.setup({
-  exclude_path_patterns = {
-    "/node_modules/",
-    ".local/share/nvim/lazy",
-  },
-  formatter_by_ft = {
-    css = formatters.lsp,
-    html = formatters.lsp,
-    java = formatters.lsp,
-    javascript = formatters.lsp,
-    json = formatters.lsp,
-    lua = formatters.lsp,
-	-- markdown = formatters.prettierd,
-    openscad = formatters.lsp,
-    python = formatters.black,
-    rust = formatters.lsp,
-    scad = formatters.lsp,
-    scss = formatters.lsp,
-    sh = formatters.shfmt,
-    terraform = formatters.lsp,
-    typescript = formatters.prettierd,
-    typescriptreact = formatters.prettierd,
-    yaml = formatters.lsp,
+	exclude_path_patterns = {
+		"/node_modules/",
+		".local/share/nvim/lazy",
+	},
+	formatter_by_ft = {
+		css = formatters.lsp,
+		html = formatters.lsp,
+		java = formatters.lsp,
+		javascript = formatters.lsp,
+		json = formatters.lsp,
+		lua = formatters.lsp,
+		-- markdown = formatters.prettierd,
+		openscad = formatters.lsp,
+		python = formatters.black,
+		rust = formatters.lsp,
+		scad = formatters.lsp,
+		scss = formatters.lsp,
+		sh = formatters.shfmt,
+		terraform = formatters.lsp,
+		typescript = formatters.prettierd,
+		typescriptreact = formatters.prettierd,
+		yaml = formatters.lsp,
 
-    -- Add conditional formatter that only runs if a certain file exists
-    -- in one of the parent directories.
-  },
+		-- Add conditional formatter that only runs if a certain file exists
+		-- in one of the parent directories.
+	},
 
-  -- By default, all shell commands are prefixed with "sh -c" (see PR #3)
-  -- To prevent that set `run_with_sh` to `false`.
-  run_with_sh = false,
+	-- By default, all shell commands are prefixed with "sh -c" (see PR #3)
+	-- To prevent that set `run_with_sh` to `false`.
+	run_with_sh = false,
 })
-
